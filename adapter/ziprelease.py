92317fd5ec816f6c85cb2d7ed61714d08bf14d46
Arjan Verkerk <arjan.verkerk@gmail.com>
2012-11-13 13:46:04 +0100

    Update zip creation script for adapters.

--- adapter/ziprelease.py
+++ adapter/ziprelease.py
@@ -11,43 +11,61 @@ import shutil
 import subprocess
 import zipfile
 
-# Get the version and latest tag from git
-describe = subprocess.check_output(shlex.split(
-    'git describe',
-)).strip()
-print('Currently at {}'.format(describe))
-lasttag = describe.split('-')[0]
-
-# checkout latest tag
-print('Check out tag {}'.format(lasttag))
-status = subprocess.call(shlex.split('git checkout {}'.format(lasttag)))
-if status:
-    print('Oops, exiting.')
-    exit()
-
-# Determine paths
-basedir = subprocess.check_output(shlex.split(
+BASEDIR = subprocess.check_output(shlex.split(
     'git rev-parse --show-toplevel',
 )).strip()
-adapterdir = os.path.join(basedir, 'adapter')
-archivepath = os.path.join(basedir, 'adapter-{}.zip'.format(lasttag))
-versionpath = os.path.join(adapterdir, 'version.txt')
+ADAPTERDIR = os.path.join(BASEDIR, 'adapter')
+VERSIONPATH = os.path.join(ADAPTERDIR, 'version.txt')
 
-# Create versionfile
-with open(versionpath, 'w') as versionfile:
-    versionfile.write('# version {}\n'.format(lasttag))
 
-# Create the zipfile
-with zipfile.ZipFile(archivepath, 'w', zipfile.ZIP_DEFLATED) as archivefile:
-    for path, dirnames, filenames in os.walk(adapterdir):
-        for filename in filenames:
-            if filename.endswith('.pyc'):
-                continue
-            archivefile.write(
-                os.path.join(path, filename),
-                os.path.join('adapter', filename),
-            )
+def get_last_tag():
+    """ Return latest tag from git. """
+    describe = subprocess.check_output(shlex.split(
+        'git describe',
+    )).strip()
+    last_tag = describe.split('-')[0]
+    return last_tag
 
-# Remove versionfile and checkout master
-os.remove(versionpath)
-subprocess.call(shlex.split('git checkout master'))
+
+def do(command):
+    """ Do command via subprocess and return result. """
+    return subprocess.call(shlex.split(command))
+
+
+def checkout(tag=None):
+    """ Checkout tag with git. """
+    if tag is None:
+        do('git checkout master')
+        do('git stash pop')
+    else:
+        do('git stash save')
+        return do('git checkout {}'.format(tag))
+   
+
+def create_version_file(versionpath, version):
+    """ Create a versionfile at filepath. """
+    with open(versionpath, 'w') as versionfile:
+        versionfile.write('# version: {}\n'.format(version))
+
+
+def create_zipfile(zippath, zipdir):
+    """ Create a zipfile at zippath and add zipdir to it. """
+    with zipfile.ZipFile(zippath, 'w', zipfile.ZIP_DEFLATED) as archivefile:
+        for path, dirnames, filenames in os.walk(zipdir):
+            for filename in filenames:
+                if filename.endswith('.pyc'):
+                    continue
+                archivefile.write(
+                    os.path.join(path, filename),
+                    os.path.join(os.path.basename(zipdir), filename),
+                )
+
+
+def main(*args, **kwargs):
+    last_tag = get_last_tag()
+    zippath = os.path.join(BASEDIR, 'adapter-{}.zip'.format(last_tag))
+    if checkout(last_tag) == 0:  # No errors
+        create_version_file(versionpath=VERSIONPATH, version=last_tag)
+        create_zipfile(zippath=zippath, zipdir=ADAPTERDIR)
+        os.remove(VERSIONPATH)
+    checkout()
