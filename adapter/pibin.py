52bd17fd983f8e5a4ab622d572dad8f5999e7f08
Arjan Verkerk <arjan.verkerk@gmail.com>
2012-11-14 12:00:05 +0100

    Add stub for reading binary files.

---
+++ adapter/pibin.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# (c) Nelen & Schuurmans.  GPL licensed, see LICENSE.rst.
+
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import absolute_import
+from __future__ import division
+
+from xml.etree import ElementTree
+
+import copy
+import datetime
+import numpy as np
+import re
+
+with open(bin_input_file)
+    arr = np.fromstring(binfile.read(), dtype=np.float32)
